2b003acbad581072c73f4efb58cbe4faf2a6db97
morien <evan.morien@gmail.com>
2017-05-10 15:48:17 -0700

    fixed outfile name

--- fastx_trim_fastq_files.sh
+++ fastx_trim_fastq_files.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#this script takes a list of files (specify with wildcard, for example. can include full or relative filepath.) and trims them using fastx toolkit to 250 base pairs
+#this script takes a list of files (specify with wildcard, for example. can include full or relative filepath.) and trims them using fastx toolkit to 250 base pairs, throwing out reads that are less than 250bp long
 #will work with either fasta or fastq files
 #DEPENDS: fastx toolkit
 
 for FILE in "$@"
 do
-    fastx_trimmer -l 250 -i $FILE -o $FILE.trimmed.fastq
+    fastx_trimmer -l 250 -i $FILE | fastx_clipper -v -a NNNNNNNNNNNNN -l 250 -o $FILE.trimmed_clipped.fastq >> fastx_trim_clip.log
 done
