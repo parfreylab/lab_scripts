b05e13465f844ddf864f66f9cc18d6e8802cf6bc
morien <evan.morien@gmail.com>
2018-12-05 14:25:51 -0800

    final draft of dada2 best practices for 18s

--- R_scripts/dada2.for_18s_data.R
+++ R_scripts/dada2.for_18s_data.R
@@ -10,7 +10,10 @@
 #best practices in our lab will involve the analysis of both merged and forward read sets separately, to identify clades excluded by any truncation/trimming done prior to merging.
 #to run this pipeline on forward reads only, just skip steps that involve the reverse reads, and skip the merging step, modifying code as necessary.
 
-#it's also necessary to filter out low-abundance ASVs after constructing the sequence table in dada2. dada2 developers do not provide any advice on this front, but two options are outlined below in the section immediately after sequence table construction
+#it's also necessary to filter out low-abundance ASVs after constructing the sequence table in dada2. dada2 developers do not provide any advice on this front, but the process is outlined below in the section after sequence table construction
+
+#our standard procedure with 18s reads processed with dada2 will be to do an analysis with both the merged reads and the R1s only, to make sure we aren't missing any important clades that have V4 regions too long to merge
+#the R1-only analysis procedure follows the merged read procedure in this guide
 
 ####Dependencies and Requirements####
 #the pipeline requires that you begin with demultiplexed fastq files (one fastq per sample)
@@ -55,21 +58,6 @@ out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, maxN=0, maxEE=c(6,8), truncQ=c(
 retained_18s <- as.data.frame(out)
 retained_18s$percentage_retained <- retained_18s$reads.out/retained_18s$reads.in*100
 View(retained_18s)
-
-####remove low-count samples####
-#mostly this needs doing so that when we reach the sample merging stage, we have no samples that have no merged reads (not allowed by dada2, prevents merging)
-#remove samples that have too few reads for merging <=50 reads (arbitrary, but low enough that we're throwing out only samples we might want to exclude on this basis)
-#actual sample filtering based on read counts and judgements about "successful" or "unsuccessful" samples can be made after the handoff to phyloseq
-#do this after trimming and filtering (creating "filtFs", "filtRs", and "out" objects)
-#remove samples that have too few reads already
-samples_to_keep <- as.numeric(out[,"reads.out"]) > 100 #or other cutoff #IMPORTANT: needs to be adjusted per-experiment, use your best judgement and remember this is a low pass filter, you don't need to throw out everything yet
-#need to redefine objects from above to exclude samples you excluded in this section
-fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))[samples_to_keep]
-fnRs <- sort(list.files(path, pattern="_R2_001.fastq", full.names = TRUE))[samples_to_keep]
-sample.names <- sapply(strsplit(basename(fnFs), "_L"), `[`, 1)
-filtFs <- file.path(path, "dada2_filtered", paste0(sample.names, "_F_filt.fastq.gz"))
-filtRs <- file.path(path, "dada2_filtered", paste0(sample.names, "_R_filt.fastq.gz"))
-out <- out[samples_to_keep,]
 
 ####learn error rates####
 errF <- learnErrors(filtFs, multithread=TRUE)
@@ -130,9 +118,8 @@ length(which(otu_pres_abs_rowsums == 1)) #how many ASVs only present in one samp
 #what are the counts of each ASV
 otu_rowsums <- rowSums(otus) #raw counts per ASV
 otu_singleton_rowsums <- as.data.frame(otu_rowsums[which(otu_pres_abs_rowsums == 1)]) #raw read counts in ASVs only presesnt in one sample
-hist(otu_singleton_rowsums[,1], breaks=10000, xlim = c(0,200), xlab="# Reads in ASV") #plot of above
-length(which(otu_singleton_rowsums <= 1)) #how many are there with more than N reads? (N=1 in example)
-length(otu_rowsums)
+hist(otu_singleton_rowsums[,1], breaks=500, xlim = c(0,200), xlab="# Reads in ASV") #histogram plot of above
+length(which(otu_singleton_rowsums <= 1)) #how many are there with N reads or fewer? (N=1 in example)
 
 #IF you want to filter out rare variants (low-read-count singleton ASVs) you can use phyloseq's "transform_sample_counts" to create a relative abundance table, and then filter your ASVs by choosing a threshold of relative abundance: otus_rel_ab = transform_sample_counts(otus, function(x) x/sum(x))
 dim(seqtab) # sanity check
@@ -177,7 +164,9 @@ NAs <- is.na(taxa[,1]) #test for NA
 NAs <- which(NAs == TRUE) #get indices of NA values
 taxa[NAs,1] <- "Unassigned" #apply new label to identified indices
 #set column ranks for the taxa table. should be Rank1 through RankN, depending on how many ranks you have. last column is the accession for our parfreylab custom-formatted databases, and species for other publicly available databases.
-colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Rank8", "Accession")
+colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Rank8", "Accession") #for parfreylab version of SIVLA 128
+colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Accession") #for parfreylab version of SIVLA 132
+colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7") #for dada2 dev's version of SIVLA 132
 
 ####saving data####
 #please note you can also save these as plain text files. Those commands will replace these when I have time to make sure I have generalizable code for it.
@@ -206,10 +195,13 @@ ps.dada2_join <- phyloseq(otu_table(seqtab.nosingletons.nochim, taxa_are_rows=FA
 #set column names for taxonomy table
 colnames(tax_table(ps.dada2_join)) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Accession")
 
+# Remove samples with less than N reads (N=100 in example. adjust per experiment.)
+ps.dada2_join <- prune_samples(sample_sums(ps.dada2_join) >= 100, ps.dada2_join)
+
 # Remove OTUs with less than N total reads. (N = 250 for example) 
 ps.dada2_join <- prune_taxa(taxa_sums(ps.dada2_join) >= 50, ps.dada2_join)
 
-# Set aside unassigned taxa #with dada2, there may not be any unassigned taxa (i think they are being removed automatically by dada2)
+# Set aside unassigned taxa #with dada2, there may not be any unassigned taxa as dada2's RDP classifier usually ends up classifying everything. You may want to adjust this command to set aside ASVs that have no assignment beyond Rank1 instead of no assignment at Rank1.
 ps.dada2_join.unassigned <- ps.dada2_join %>%
   subset_taxa(Rank1 == "Unassigned")
 # Remove unassigned taxa
@@ -220,4 +212,125 @@ ps.dada2_join <- ps.dada2_join %>%
 otu <- as.data.frame(otu_table(ps.dada2_join))
 otu_table(ps.dada2_join)[otu <= 1] <- 0
 
-##TODO: remove samples that have fewer than N reads
+#after denoising you can also remove ASVs that are in groups you wish to exclude (i.e. mammalia, embryophyta, etc.)
+#to do this, just determine which rank the clade is captured by, and filter like so:
+# Remove mitochondrial and chloroplast OTUs
+ps.dada2_join <- ps.dada2_join %>%
+  subset_taxa(Rank5 != "Mammalia") %>% #you can chain as many of these subset_taxa calls as you like into this single command using the R pipe (%>%)
+  subset_taxa(Rank5 != "Embryophyta")
+
+#with your filtered phyloseq object, you are now ready to move on to whatever statistical/diversity analyses you're planning to do. please see other R script guides in the lab github.
+
+
+
+####R1-ONLY ANALYSIS GUIDE####
+####File Path Setup####
+#this is so dada2 can quickly iterate through all the R1 and R2 files in your read set
+path <- "/projects/my_project/18s/data/raw_data/" # CHANGE ME to the directory containing the fastq files after unzipping.
+list.files(path)
+fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
+
+####fastq Quality Plots####
+plotQualityProfile(fnFs[1:20]) #this plots the quality profiles for each sample, if you have a lot of samples, it's best to look at just a few of them, the plots take a minute or two to generate even only showing 10-20 samples.
+
+####prepare file names for filtering####
+#this defines names and paths for the filtered fastq files you are about to generate
+filtFs <- file.path(path, "dada2_filtered", paste0(sample.names, "_F_filt.fastq"))
+
+####filter and trim reads####
+#adjusting the parameters for the filterAndTrim function are crucial to the success of a dada2 run. truncation length, in particular, will be a strong determinant of the percentage of reads you retain per sample
+#the parameters below represent best-practices from several different 18s experiments that we have done, tips from Ramon Masanna's lab, and parameters have observed in published literature. the truncation length will need to be adjusted for the sequencing technology, of course. this example truncation length is OK for MiSeq data.
+out <- filterAndTrim(fnFs, filtFs, maxN=0, maxEE=c(6), truncQ=c(2), rm.phix=TRUE, compress=TRUE, verbose=TRUE, truncLen=c(240), multithread = TRUE, matchIDs=TRUE) #with trunQ filter
+retained_R1s <- as.data.frame(out)
+retained_R1s$percentage_retained <- retained_R1s$reads.out/retained_R1s$reads.in*100
+View(retained_R1s)
+
+####learn error rates####
+errF <- learnErrors(filtFs, multithread=TRUE)
+plotErrors(errF, nominalQ=TRUE) #assess this graph. it shows the error rates observed in your dataset. strange or unexpected shapes in the plot should be considered before moving on.
+
+####dereplication####
+derepFs <- derepFastq(filtFs, verbose=TRUE)
+
+# Name the derep-class objects by the sample names #this is just to ensure that all your R objects have the same sample names in them
+names(derepFs) <- sample.names
+
+####sample inference####
+dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
+dadaFs[[1]]
+
+####construct sequence table####
+seqtab <- makeSequenceTable(dadaFs)
+dim(seqtab)
+
+####remove low-count singleton ASVs####
+#create phyloseq otu_table
+otus <- otu_table(t(seqtab), taxa_are_rows = TRUE)
+
+#some metrics from the sequence table
+otu_pres_abs <- otus
+otu_pres_abs[otu_pres_abs >= 1] <- 1 #creating a presence/absence table
+otu_pres_abs_rowsums <- rowSums(otu_pres_abs) #counts of sample per ASV
+length(otu_pres_abs_rowsums) #how many ASVs
+length(which(otu_pres_abs_rowsums == 1)) #how many ASVs only present in one sample
+
+#what are the counts of each ASV
+otu_rowsums <- rowSums(otus) #raw counts per ASV
+otu_singleton_rowsums <- as.data.frame(otu_rowsums[which(otu_pres_abs_rowsums == 1)]) #raw read counts in ASVs only presesnt in one sample
+hist(otu_singleton_rowsums[,1], breaks=500, xlim = c(0,200), xlab="# Reads in ASV") #histogram plot of above
+length(which(otu_singleton_rowsums <= 1)) #how many are there with N reads or fewer? (N=1 in example)
+
+#IF you want to filter out rare variants (low-read-count singleton ASVs) you can use phyloseq's "transform_sample_counts" to create a relative abundance table, and then filter your ASVs by choosing a threshold of relative abundance: otus_rel_ab = transform_sample_counts(otus, function(x) x/sum(x))
+dim(seqtab) # sanity check
+dim(otus) # (this should be the same as last command, but the dimensions reversed)
+otus_rel_ab <- transform_sample_counts(otus, function(x) x/sum(x)) #create relative abundance table
+df <- as.data.frame(unclass(otus_rel_ab)) #convert to plain data frame
+df[is.na(df)] <- 0 #if there are samples with no merged reads in them, and they passed the merge step (a possiblity, converting to a relative abundance table produes all NaNs for that sample. these need to be set to zero so we can do the calculations in the next steps.)
+otus_rel_ab.rowsums <- rowSums(df) #compute row sums (sum of relative abundances per ASV. for those only present in one sample, this is a value we can use to filter them for relative abundance on a per-sample basis)
+a <- which(as.data.frame(otu_pres_abs_rowsums) == 1) #which ASVs are only present in one sample
+b <- which(otus_rel_ab.rowsums <= 0.001) #here is where you set your relative abundance threshold #which ASVs pass our filter for relative abundance
+length(intersect(a,b)) #how many of our singleton ASVs fail on this filter
+rows_to_remove <- intersect(a,b) #A also in B (we remove singleton ASVs that have a lower relative abundance value than our threshold)
+otus_filt <- otus[-rows_to_remove,] #filter OTU table we created earlier
+dim(otus_filt) #how many ASVs did you retain?
+seqtab.nosingletons <- t(as.matrix(unclass(otus_filt))) #convert filtered OTU table back to a sequence table matrix to continue with dada2 pipeline
+
+####remove chimeras####
+#here we remove "bimeras" or chimeras with two sources. look at "method" to decide which type of pooling you'd like to use when judging each sequence as chimeric or non-chimeric
+seqtab.nosingletons.nochim <- removeBimeraDenovo(seqtab.nosingletons, method="pooled", multithread=TRUE, verbose=TRUE) #this step can take a few minutes to a few hours, depending on the size of your dataset
+dim(seqtab.nosingletons.nochim)
+sum(seqtab.nosingletons.nochim)/sum(seqtab.nosingletons) #proportion of nonchimeras #it should be relatively high after filtering out your singletons/low-count ASVs, even if you lose a lot of ASVs, the number of reads lost should be quite low
+
+####track read retention through steps####
+getN <- function(x) sum(getUniques(x))
+track <- cbind(out[samples_to_keep,], sapply(dadaFs[samples_to_keep], getN), sapply(dadaRs[samples_to_keep], getN), sapply(mergers, getN), rowSums(seqtab.nosingletons), rowSums(seqtab.nosingletons.nochim))
+# If processing only a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
+track <- cbind(track, 100-track[,4]/track[,3]*100, 100-track[,5]/track[,4]*100)
+colnames(track) <- c("input", "filtered", "denoisedF", "nosingletons", "nochimeras", "percent_singletons", "percent_chimeras")
+rownames(track) <- sample.names
+write.table(track, "read_retention.merged_18s_R1s.txt", quote=F, row.names=T, col.names=T, sep="\t")
+
+####assign taxonomy####
+#note, this takes ages if you have a large dataset. strongly recommend doing on a multi-core machine (zoology cluster, or entamoeba in the lab). another option: saving the sequences as a fasta file (with writeFasta) and using QIIME's taxonomy assignment command will save you time, and is only slightly less accurate than the dada2 package's taxonomy assignment function (their implementation of RDP).
+#for 18s mammalian gut experiments
+taxa <- assignTaxonomy(seqtab.nosingletons.nochim, "/PATH/TO/taxonomy_databases/silva_for_dada2/v128_for_parfreylab/18s/silva_128.18s.99_rep_set.dada2.fa.gz", multithread=TRUE)
+#for other 18s experiments
+taxa <- assignTaxonomy(seqtab.nosingletons.nochim, "/PATH/TO/taxonomy_databases/silva_for_dada2/v132_for_parfreylab/18s/silva_132.18s.99_rep_set.dada2.fa.gz", multithread=TRUE)
+
+#NA taxa are hard to separate later if they have no label. apply "Unassigned" label here now.
+unique(taxa[,1]) #possible labels here: eukaryotic, archaeal, bacterial, and "NA" taxa. 
+NAs <- is.na(taxa[,1]) #test for NA
+NAs <- which(NAs == TRUE) #get indices of NA values
+taxa[NAs,1] <- "Unassigned" #apply new label to identified indices
+#set column ranks for the taxa table. should be Rank1 through RankN, depending on how many ranks you have. last column is the accession for our parfreylab custom-formatted databases, and species for other publicly available databases.
+colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Rank8", "Accession") #for parfreylab version of SIVLA 128
+colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7", "Accession") #for parfreylab version of SIVLA 132
+colnames(taxa) <- c("Rank1", "Rank2", "Rank3", "Rank4", "Rank5", "Rank6", "Rank7") #for dada2 dev's version of SIVLA 132
+
+####saving data####
+#please note you can also save these as plain text files. Those commands will replace these when I have time to make sure I have generalizable code for it.
+write.table(taxa, "taxonomy_table.18s_R1s.txt", sep="\t", quote=F, row.names=T, col.names=T)
+write.table(seqtab.nosingletons.nochim, "sequence_table.18s_R1s.txt", sep="\t", quote=F, row.names=T, col.names=T)
+
+#the handoff to phyloseq remains the same for both R1 and merged procedures, see above section.
